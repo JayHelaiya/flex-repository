97ad271560dc661903af7ebee17d725595f6c478
unknown <jayhelaiya007@gmail.com>
2020-05-10 14:52:13 +0530

    first demo

---
+++ flex-demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './form-flex-angular.html',
+  styleUrls: ['./form-flex-angular.css']
+})
+export class AppComponent {
+  title = 'flex-demo';
+}
